61c3f78ff7b8c3cf7897edd7e8c58064251c6de0
davi.carneiro <dcarneir@vld583.cmpd1.metoffice.gov.uk>
2020-08-19 11:12:39 +0100

    Removing temporary files (not needed anymore)

--- HL_error_covs/call_errorCovs_HL.py
+++
@@ -1,9 +0,0 @@
-import glob
-import master
-
-input_dir = "/data/users/dcarneir/data/ncfiles/output_error_covs"
-files = input_dir + '/' + 'test_accum_stats*.nc'
-list_of_files = sorted(glob.glob(files))
-outfilename = '/data/users/dcarneir/data/ncfiles/output_error_covs/test_errorcovs.nc'
-master.HL_error_covs(list_of_files, outfilename=outfilename)
-
